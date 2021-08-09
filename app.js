712a68228ddc3801d1b96647e10365f858d11207
Queeniva <ngulefacbenazea@gmail.com>
2021-08-09 18:45:12 +0100

    added mongodb backend to my chat application

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+const mongoose = require ('mongoose');
+mongoose.connect('mongodb://localhost:27017/chatAppDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const chatSchema = {
+  message:String
+}
+const Chat = mongoose.model("Chat", chatSchema)
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -10,6 +18,10 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
+    const text =  new Chat({
+      message:msg
+    })
+    text.save();
   });
 });
 
